dac04a995298930c5f5d5ca7daf854fc8dbe4a2a
UsboKirishima <usbertibox@gmail.com>
2022-03-13 18:27:30 +0100

    feat: from ts to js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
+document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
